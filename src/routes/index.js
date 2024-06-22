fde9c0933fadea74a0d50fcb2eef38489507d581
NguyenDuyHieu987 <104075342+NguyenDuyHieu987@users.noreply.github.com>
2024-06-22 19:41:38 +0700

    chore: update

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,18 @@
+import apicache from 'apicache';
 import createHttpError from 'http-errors';
 import videosRouter from './video.route.js';
 import imageRouter from './image.route.js';
 import ErrorHandler from '../controllers/error.controller.js';
 import { proxyHandler } from '../middlewares/index.js';
+import RedisCache from '../config/redis/index.js';
+
+const cacheWithRedis = apicache.options({
+  redisClient: RedisCache,
+}).middleware;
 
 export default function route(app) {
-  app.use('/images', imageRouter.Get);
-  app.use('/videos', videosRouter.Get);
+  app.use('/images', cacheWithRedis('1 hours'), imageRouter.Get);
+  app.use('/videos', cacheWithRedis('2 hours'), videosRouter.Get);
   app.use(proxyHandler);
   app.use('/image', imageRouter.Service);
   app.use('/video', videosRouter.Service);
