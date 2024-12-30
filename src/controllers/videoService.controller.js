426c8daf2489f4cd0dd93e24057bf8aba5433d3a
NguyenDuyHieu987 <104075342+NguyenDuyHieu987@users.noreply.github.com>
2024-12-30 18:33:54 +0700

    chore: update

--- src/controllers/videoService.controller.js
+++ src/controllers/videoService.controller.js
@@ -61,53 +61,79 @@ class VideoServiceController {
           'C:\\Users\\ddom6\\Downloads\\ffmpeg-2024-09-19-git-0d5b68c27c-full_build\\bin\\ffmpeg.exe'
           // 'C:\\Users\\ddom6\\Downloads\\ffmpeg-7.1-full_build\\bin\\ffmpeg.exe'
         );
+        ffmpegTool.setFfprobePath(
+          'C:\\Users\\ddom6\\Downloads\\ffmpeg-2024-09-19-git-0d5b68c27c-full_build\\bin\\ffprobe.exe'
+        );
       }
 
       const videoName = `${originalFileName}_${generateRandomString(20)}_.m3u8`;
 
-      ffmpegTool
-        .output(`${outputDir}/${videoName}`)
-        .addOptions([
-          '-profile:v baseline', // Tương thích cho phát trực tuyến
-          '-level 3.0',
-          '-start_number 0',
-          '-hls_time 1', // Mỗi phân đoạn có thời lượng 10 giây
-          '-hls_list_size 0', // Lưu toàn bộ danh sách
-          '-f hls', // Định dạng đầu ra là HLS
-        ])
-        .on('progress', (progress) => {
-          const percent = (progress.percent || 0).toFixed(2);
-          SocketService.emitToAll('upload-video-progress', {
-            percent: +percent,
-          });
-        })
-        .on('end', async () => {
-          // const metadata = ffmpegTool.ffprobe();
+      ffmpegTool.ffprobe((err, metadata) => {
+        if (err) {
+          console.error('Error reading video metadata:', err);
+          return;
+        }
+        req.file.metadata = metadata;
+        req.file.duration = metadata.format.duration;
 
-          ffmpegTool.ffprobe((err, metadata) => {
-            if (err) {
-              console.error('Error reading video metadata:', err);
-              return;
-            }
+        // const timestamps = Array.from(
+        //   { length: Math.ceil(metadata.format.duration) },
+        //   (_, i) => i
+        // );
 
-            req.file.metadata = metadata;
-            req.file.duration = metadata.format.duration;
+        // ffmpegTool.thumbnails({
+        //   timestamps: [1],
+        //   filename: `preview_%s.jpg`,
+        //   folder: path.join(__dirname, `src/public/imagesTest/still1`),
+        //   size: '320x240',
+        // });
 
+        ffmpegTool
+          .output(`${outputDir}/${videoName}`)
+          .addOptions([
+            '-profile:v baseline', // Tương thích cho phát trực tuyến
+            '-level 3.0',
+            '-start_number 0',
+            '-hls_time 1', // Mỗi phân đoạn có thời lượng 10 giây
+            '-hls_list_size 0', // Lưu toàn bộ danh sách
+            '-f hls', // Định dạng đầu ra là HLS
+          ])
+          // .screenshot({
+          //   timestamps: [1],
+          //   filename: `preview_%s.jpg`,
+          //   folder: path.join(__dirname, `src/public/imagesTest/still1`),
+          //   size: '320x240',
+          // })
+          // .screenshots({
+          //   timestamps: [0, 1, 2],
+          //   filename: `preview_%s.jpg`,
+          //   folder: path.join(__dirname, `src/public/imagesTest/preview`),
+          //   size: '320x240',
+          // })
+          .on('progress', (progress) => {
+            const percent = (progress.percent || 0).toFixed(2);
+            SocketService.emitToAll('upload-video-progress', {
+              percent: +percent,
+            });
+          })
+          .on('end', async () => {
             res.json({
               success: true,
               message: 'Video uploaded successfully!',
               file: req.file,
               video_path: `${folder}/${videoDirectory}/${videoName}`,
             });
-          });
-        })
-        .on('error', (err) => {
-          res.json({
-            success: false,
-            message: 'Video upload failed!',
-          });
-        })
-        .run();
+          })
+          .on('error', (err) => {
+            console.error(err);
+
+            res.json({
+              success: false,
+              message: 'Video upload failed!',
+            });
+          })
+          .run();
+      });
     } catch (error) {
       next(error);
     } finally {
